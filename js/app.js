bc2d0713c4c4c9e5db0f632e369521544076dd04
tektechnologies <info@tektechnologies.com>
2023-09-25 11:22:36 -0500

    add problem domain comment

--- js/app.js
+++ js/app.js
@@ -1,2 +1,62 @@
 'use strict';
 console.log('app file is connected');
+
+/*
+  Practice domain modeling by planning out an app w that allows users to choose their favorite between two pizzas
+
+
+  App Flow:
+  - Randomly put 2 pizzas on the screen
+    - Random number generator
+    - a function to display pizzas
+  - A user clicks on a pizza
+    - event listener needs to be on the image to fire the event handler
+    - the event handler fires
+      - ? check if total clicks is 5 ?
+        - stop letting the user click
+        - display the clicks
+      -? If not ?
+        - track which pizza was clicked on
+        - update click images count of how many times it has been clicked on
+        - update both images'count of times shown
+        - Randomly Pick 2 pizzas, run the same code that put them on the screen to begin with
+
+  HTML
+    - have a left and right image container in the html
+    - Give them id's so we can select them// in demo today we are grab DOM elements differently.
+    - let the user know what they are supposed to do
+      - instructions
+
+  More javascript details
+  We need Objects with all the image properties
+  const Image = function ()
+  {
+    name : 'Chicago Pizza',
+    clicks: 0,
+    timesShown: 0,
+    url : 'chicagoPizza.jpeg'
+  };
+
+  We need an Array to hold all image Objects
+
+  function to randomly pick an image{
+    Prevent last picked pizzas from being picked
+      - STRETCH pick all pizzas evenly as possible
+    Math.floor  Math.random() * array.length()
+    make sure left and right image are unique
+  }
+
+  click on an image, targetted by id
+  add event listener('click', function(){
+    keep track of the image that is clicked
+    prevent all currently displayed images from being re clicked {
+    }
+  })
+
+  function to display all the clicks at the end () {
+    generate a table or ul list
+    populate the data - adds to the inner html of an existing element (table or list)
+    divide object's times clicked by total shown
+  }
+
+*/
